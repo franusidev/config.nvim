279843df1e79b5cd4981424862d6a89240fb4d24
Francisco Nunez Sierra <franusidev@gmail.com>
2026-05-15 01:10:04 +0200

    fix: add default options for tabs and clipboard

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 require("config.lazy")
+
+-- tabs transform to spaces and have 4 characters lenght
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+-- make the unnamed registry the system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+
+
