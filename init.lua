d982dc77ed8b751021d95cc69e3b213ae0262530
Francisco Nunez Sierra <franusidev@gmail.com>
2026-05-16 16:36:24 +0200

    feat: a highlight to yanked text

--- init.lua
+++ init.lua
@@ -7,7 +7,16 @@ vim.opt.softtabstop = 4
 -- make the unnamed registry the system clipboard
 vim.opt.clipboard = 'unnamedplus'
 
-
+-- enable virtual text for diagnostics
 vim.diagnostic.config({
 	virtual_text = true
+})
+
+-- add a yank highlight
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
