69c540daeeb8e39635f4b751eb3b11528c4bca2d
Francisco Nunez Sierra <franusidev@gmail.com>
2026-05-17 13:50:39 +0200

    fix: add keymaps to - and _ to open oil, and enable preview by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -21,6 +21,15 @@ return {
 			vim.keymap.set("n", "_", function()
 				require("oil").open(vim.fn.expand("%:p:h"))
 			end, { desc = "explore files in current directory" })
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "OilEnter",
+				callback = vim.schedule_wrap(function(args)
+					local oil = require("oil")
+					if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+						oil.open_preview()
+					end
+				end),
+			})
 		end
 	}
 }
